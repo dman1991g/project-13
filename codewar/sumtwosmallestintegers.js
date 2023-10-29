f27c5ce8023855d0a82569ccbb62909503df26a1
dman1991g <141966927+dman1991g@users.noreply.github.com>
2023-10-28 23:38:23 -0400

    Create sumtwosmallestintegers.js

---
+++ codewar/sumtwosmallestintegers.js
@@ -0,0 +1,9 @@
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  
+  numbers = numbers.sort(function (a, b) { //the numbers in an array need to be sorted
+    return a - b;
+  });
+  
+  return numbers[0] + numbers[1];//once they are sorted then the first two numbers can be added together
+};
