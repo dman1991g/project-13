0d52b0f5095a82e740fca29b448b5a07e7a270eb
dman1991g <141966927+dman1991g@users.noreply.github.com>
2023-10-28 22:47:01 -0400

    Create willhesurvive.js
    created codewars folder and js file

---
+++ codewar/willhesurvive.js
@@ -0,0 +1,7 @@
+Function:hero(bullets,dragon){
+    if(bullets/2>=dragons) // Since each dragon takes 2 bullets we need to make sure he has enough
+    return = "true";
+}else{
+    (bullets/2<dragons)
+    return = "false";
+}
